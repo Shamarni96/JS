8b70d2e65d4dd0ba1fecdfd62556a994514bec30
shamarni mcfarlane <Smcfarlane926@gmail.com>
2020-06-02 15:01:07 +0100

    unit testing

--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
@@ -15,3 +15,19 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+test('divide 6 / 3 to equal 2', () => {
+  expect(calculator.divide(6, 3)).toBe(2);
+})
+
+test('divide 20 / 5 to equal 4', () => {
+  expect(calculator.divide(20, 5)).toBe(4);
+})
+
+test('multiply 5 * 3 to equal 15', () => {
+  expect(calculator.multiply(5, 3)).toBe(15);
+})
+
+test('multiply 9 * 12 to equal 108', () => {
+  expect(calculator.multiply(9, 12)).toBe(108);
+})
--- JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
+++ JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
@@ -16,4 +16,21 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+  test('add user to userController', () => {    
+    let user = new User(1224,"Seb", "seb@generation.org");
+    userController.add(user);    
+    expect(userController.getUsers()).toContain(user);
+  });
+
+  test('remove user to userController', () => {    
+    let user = new User(1224,"Seb", "seb@generation.org");
+    userController.add(user);    
+    userController.remove(user);
+    expect(userController.users).not.toContain(user);
+  });
+
+  test('find user by email ', () => {
+    userController.findByEmail('santiago@generation.org');
+    expect(userController.findByEmail('santiago@generation.org')).toEqual({"email": "santiago@generation.org", "id": 1234, "name": "Santiago"});
+  });
 
